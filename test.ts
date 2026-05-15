806be1466952d1447c3b58c97b01ff26eb84b142
Orkun <wyntinedev@gmail.com>
2026-05-16 00:55:13 +0300

    test.ts updated

--- test.ts
+++ test.ts
@@ -1,9 +1,12 @@
 import { mergeArraysToObjectArray } from "./src/lib/helpers/objects";
 
-const result = mergeArraysToObjectArray({ index: ["1", "2"], category: ["abc", "def"], });
+const result = mergeArraysToObjectArray({
+  index: ["1", "2"],
+  category: ["abc", "def"],
+});
 
 console.log(result);
 
-const result2 = mergeArraysToObjectArray({ index: ["a", "b"], });
+const result2 = mergeArraysToObjectArray({ index: ["a", "b"] });
 
 console.log(result2);
